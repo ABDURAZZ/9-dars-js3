048cbf32f1327d9fb994f27bd7ce909fbd618c75
ABDURAZZ <abdurazzokbossov@gmail.com>
2024-09-23 18:37:46 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,21 @@
+let tugma1 = document.querySelector('button')
+let tugma2 = document.querySelector('button')
+let input = document.querySelector('input')
+
+let box = document.querySelector('.box')
+tugma2.addEventListener('click', ()=>{
+    (input.value == '123456')
+    alert('id')
+     main.style.displey = 'none'
+        main.style.displey = 'bleck'
+} )
+tugma1.addEventListener('click', () => {
+    if (input.value == '78900') {
+        alert('baratan qale')
+        main.style.displey = 'none'
+        main.style.displey = 'bleck'
+    } else {
+        alert('yoq')
+    }
+    input.vale = ''
+})
